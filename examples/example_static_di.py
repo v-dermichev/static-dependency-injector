0e90258eb420537a8fa0f10adb0f9ce51caf8b95
vdermichev <v.dermichev@gmail.com>
2026-07-01 19:53:01 +0200

    [Fix] removed unneeded import

--- examples/example_static_di.py
+++ examples/example_static_di.py
@@ -1,5 +1,3 @@
-from pydantic import BaseModel, Field
-
 from examples.example_services.http_client_service import HttpClientService
 from examples.example_services.logger_service import LoggerService
 from examples.example_services.user_provider_service import UserProviderService
@@ -12,9 +10,3 @@ class ExampleStaticDI(StaticDeclarativeContainer):
     http_client = Factory(HttpClientService, logger=logger)
     user_provider = Singleton(UserProviderService, http_client=http_client, logger=logger)
 
-
-ExampleStaticDI.clear_overrides("http_client")
-
-
-class M(BaseModel):
-    a = Field()
